58805daa2492c9fb05c12980c160ac6f700ccc85
Benill0 <elbenitope@gmail.com>
2024-06-01 20:27:11 -0500

    feat: :rocket: deploy git pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://benill0.github.io/to-do-list',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "./src"),
